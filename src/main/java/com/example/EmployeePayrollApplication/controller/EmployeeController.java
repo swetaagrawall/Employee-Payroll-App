95574cb5492291c5754f332006eb99a0d6931c3a
sweta <swetaarawal972@gmail.com>
2025-02-25 13:59:47 +0530

    [Sweta Agrawal]Add.uc1-Rest Controller with http methods is added sucessfully.

--- src/main/java/com/example/EmployeePayrollApplication/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeePayrollApplication/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.EmployeePayrollApplication.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employee")
@@ -10,6 +11,21 @@ public class EmployeeController {
     @GetMapping("/welcome")
     public String welcomeMessage() {
         return "Welcome to Employee Payroll App!";
+    }
+
+    @PostMapping("/add")
+    public String addEmployee() {
+        return "Employee added successfully!";
+    }
+
+    @PutMapping("/update")
+    public String updateEmployee() {
+        return "Employee updated successfully!";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteEmployee() {
+        return "Employee deleted successfully!";
     }
 }
 
