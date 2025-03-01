15eb56ae9c4c3579d23687c112edd61e0e101a55
sweta <swetaarawal972@gmail.com>
2025-03-01 13:12:21 +0530

    [Sweta Agrawal]Add.uc11_UserFriendlyException is added sucessfully

--- src/main/java/com/example/employeepayrollapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/employeepayrollapplication/exception/GlobalExceptionHandler.java
@@ -8,17 +8,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    //UC11: Handle Validation Errors (e.g., Invalid Name Format)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage())
-        );
-        return errors;
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+        errorResponse.put("error", "Validation Error");
+        errorResponse.put("errors", errors);
+
+        return ResponseEntity.badRequest().body(errorResponse);
     }
+
 }
