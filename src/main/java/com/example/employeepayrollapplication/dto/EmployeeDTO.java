d00f1faceebbf10287ae6a2422ae35929a02463d
sweta <swetaarawal972@gmail.com>
2025-03-01 10:26:17 +0530

    [Sweta Agrawal]Add.uc6-_LombokUsed is added sucessfully

--- src/main/java/com/example/employeepayrollapplication/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeepayrollapplication/dto/EmployeeDTO.java
@@ -1,27 +1,14 @@
 package com.example.employeepayrollapplication.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
     private String name;
-    private double salary;
+    private long salary;
 
-    public EmployeeDTO(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
--- src/main/java/com/example/employeepayrollapplication/model/Employee.java
+++ src/main/java/com/example/employeepayrollapplication/model/Employee.java
@@ -1,45 +1,22 @@
 package com.example.employeepayrollapplication.model;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
-    private String department;
+    @Column(name = "salary", nullable = false)
     private double salary;
 
-    public Employee() {}
-
-    public Employee(String name, String department, double salary) {
-        this.name = name;
-        this.department = department;
-        this.salary = salary;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getDepartment() { return department; }
-    public void setDepartment(String department) { this.department = department; }
-
-    public double getSalary() { return salary; }
-    public void setSalary(double salary) { this.salary = salary; }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", department='" + department + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
 }
