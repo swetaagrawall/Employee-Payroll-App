c8b7fb2be933907c73a9a87d03e34a13743806e2
sweta <swetaarawal972@gmail.com>
2025-03-01 10:45:25 +0530

    [Sweta Agrawal]Add. uc8_LoggingLevels is added sucessfully

--- src/main/java/com/example/employeepayrollapplication/EmployeePayrollApplication.java
+++ src/main/java/com/example/employeepayrollapplication/EmployeePayrollApplication.java
@@ -3,11 +3,15 @@ package com.example.employeepayrollapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll app started...", context.getEnvironment().getProperty("enviroment"));
 	}
-
 }
